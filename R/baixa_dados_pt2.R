37bf39507b8f6c8b278a85e0cc0569c5ca301feb
Rafael Herzog <rafasantanah21@gmail.com>
2024-06-28 19:48:52 -0300

    atualização junho 2024

--- R/baixa_dados_pt2.R
+++ R/baixa_dados_pt2.R
@@ -51,14 +51,39 @@ df_aux_municipios <- df_aux_municipios |>
   arrange(CODMUNRES)
 
 
-# Baixando os dados preliminares do SINASC de 2022 ------------------------
-dados_preliminares_2022_aux <- fetch_datasus(
-  year_start = 2022, 
-  year_end = 2022,
-  information_system = "SINASC"
-)
+# Baixando os dados preliminares do SINASC de 2023 ------------------------
+download.file("https://s3.sa-east-1.amazonaws.com/ckan.saude.gov.br/SINASC/DNOPEN23.csv", "R/databases/DNOPEN23.csv", mode = "wb")
 
-dados_preliminares_2022 <- left_join(dados_preliminares_2022_aux, df_aux_municipios) |>
+dados_preliminares_2022_aux1 <- fetch_datasus(
+  year_start = 2022, 
+  year_end = 2023,
+  information_system = "SINASC"
+) 
+
+## Por enquanto, o microdatasus só tem dados até 2022
+unique(substr(dados_preliminares_2022_aux1$DTNASC, nchar(dados_preliminares_2022_aux1$DTNASC) - 3, nchar(dados_preliminares_2022_aux1$DTNASC)))
+
+## Lendo os dados preliminares de 2023, baixados do opendatasus
+dados_preliminares_2023_aux1 <- read.csv2("R/databases/DNOPEN23.csv")
+file.remove("R/databases/DNOPEN23.csv")
+
+## Checando se as colunas são as mesmas
+names(dados_preliminares_2023_aux1)[which(!(names(dados_preliminares_2023_aux1) %in% names(dados_preliminares_2022_aux1)))]
+names(dados_preliminares_2022_aux1)[which(!(names(dados_preliminares_2022_aux1) %in% names(dados_preliminares_2023_aux1)))]
+
+## Retirando as variáveis que não batem
+dados_preliminares_2022_aux2 <- dados_preliminares_2022_aux1 |>
+  select(!CONTADOR) 
+
+dados_preliminares_2023_aux2 <- dados_preliminares_2023_aux1 |>
+  select(!c(contador, OPORT_DN)) |>
+  mutate_if(is.numeric, as.character)
+
+## Juntando as duas bases
+dados_preliminares_2023_aux <- full_join(dados_preliminares_2022_aux2, dados_preliminares_2023_aux2)
+
+## Transformando algumas variáveis
+dados_preliminares_2023 <- left_join(dados_preliminares_2023_aux, df_aux_municipios) |>
   mutate(
     ano_nasc = as.numeric(substr(DTNASC, nchar(DTNASC) - 3, nchar(DTNASC))), 
     .after = DTNASC, 
@@ -79,7 +104,7 @@ dados_preliminares_2022 <- left_join(dados_preliminares_2022_aux, df_aux_municip
 # Número de nascidos vivos ------------------------------------------------
 df_nascidos_2021 <- read.csv2("R/databases/Nascimentos_muni2021.csv")
 
-df_nascidos_aux <- dados_preliminares_2022 |>
+df_nascidos_aux <- dados_preliminares_2023 |>
   select(res_SIGLA_UF, res_MUNNOMEX, CODMUNRES, ano_nasc) |>
   mutate(nascidos = 1) |>
   group_by(res_SIGLA_UF, res_MUNNOMEX, CODMUNRES, ano_nasc) |>
@@ -88,17 +113,17 @@ df_nascidos_aux <- dados_preliminares_2022 |>
 
 names(df_nascidos_aux) <- c('UF', 'Municipio', 'Codigo', 'Ano', 'Nascidos')
 
-df_nascidos_2022 <- full_join(df_nascidos_2021, df_nascidos_aux) |>
+df_nascidos_2023 <- full_join(df_nascidos_2021, df_nascidos_aux) |>
   arrange(UF, Municipio)
 
 # Exportando os dados 
-write.table(df_nascidos_2022, 'R/databases/Nascimentos_muni2022.csv', sep = ";", dec = ".", row.names = FALSE)
+write.table(df_nascidos_2023, 'R/databases/Nascimentos_muni2023.csv', sep = ";", dec = ".", row.names = FALSE)
 
 
 # Nascidos vivos prematuros -----------------------------------------------
 df_prematuros_2021 <- read.csv2("R/databases/Prematuridade_muni2021.csv")
 
-df_prematuros_aux <- dados_preliminares_2022 |>
+df_prematuros_aux <- dados_preliminares_2023 |>
   select(res_SIGLA_UF, res_MUNNOMEX, CODMUNRES, ano_nasc, GESTACAO) |>
   mutate(nascidos = 1) |>
   group_by(res_SIGLA_UF, res_MUNNOMEX, CODMUNRES, ano_nasc, GESTACAO) |>
@@ -110,16 +135,16 @@ names(df_prematuros_aux) <- c('UF', 'Municipio', 'Codigo', 'Ano', 'Gestacao', 'N
 sort(unique(df_prematuros_2021$Gestacao), na.last = TRUE)
 sort(unique(df_prematuros_aux$Gestacao), na.last = TRUE)
 
-df_prematuros_2022 <- full_join(df_prematuros_2021, df_prematuros_aux)
+df_prematuros_2023 <- full_join(df_prematuros_2021, df_prematuros_aux)
 
 # Exportando os dados (Nascimentos_muni = UF, Município, Ano e Nascimentos)
-write.table(df_prematuros_2022, 'R/databases/Prematuridade_muni2022.csv', sep = ";", dec = ".", row.names = FALSE)
+write.table(df_prematuros_2023, 'R/databases/Prematuridade_muni2023.csv', sep = ";", dec = ".", row.names = FALSE)
 
 
 # Tipo de gestação --------------------------------------------------------
 df_tipo_gravidez_2021 <- read.csv2("R/databases/Tipo_gravidez_muni2021.csv")
 
-df_tipo_gravidez_aux <- dados_preliminares_2022 |>
+df_tipo_gravidez_aux <- dados_preliminares_2023 |>
   select(res_SIGLA_UF, res_MUNNOMEX, CODMUNRES, ano_nasc, GRAVIDEZ) |>
   mutate(nascidos = 1) |>
   group_by(res_SIGLA_UF, res_MUNNOMEX, CODMUNRES, ano_nasc, GRAVIDEZ) |>
@@ -131,16 +156,16 @@ names(df_tipo_gravidez_aux) <- c('UF', 'Municipio', 'Codigo', 'Ano', 'Gravidez',
 sort(unique(df_tipo_gravidez_2021$Gravidez), na.last = TRUE)
 sort(unique(df_tipo_gravidez_aux$Gravidez), na.last = TRUE)
 
-df_tipo_gravidez_2022 <- full_join(df_tipo_gravidez_2021, df_tipo_gravidez_aux)
+df_tipo_gravidez_2023 <- full_join(df_tipo_gravidez_2021, df_tipo_gravidez_aux)
 
 # Exportando os dados 
-write.table(df_tipo_gravidez_2022, 'R/databases/Tipo_gravidez_muni2022.csv', sep = ";", dec = ".", row.names = FALSE)
+write.table(df_tipo_gravidez_2023, 'R/databases/Tipo_gravidez_muni2023.csv', sep = ";", dec = ".", row.names = FALSE)
 
 
 # Tipo de parto -----------------------------------------------------------
 df_tipo_parto_2021 <- read.csv2("R/databases/Tipo_parto_muni2021.csv")
 
-df_tipo_parto_aux <- dados_preliminares_2022 |>
+df_tipo_parto_aux <- dados_preliminares_2023 |>
   select(res_SIGLA_UF, res_MUNNOMEX, CODMUNRES, ano_nasc, PARTO) |>
   mutate(nascidos = 1) |>
   group_by(res_SIGLA_UF, res_MUNNOMEX, CODMUNRES, ano_nasc, PARTO) |>
@@ -152,16 +177,16 @@ names(df_tipo_parto_aux) <- c('UF', 'Municipio', 'Codigo', 'Ano', 'Parto', 'Nasc
 sort(unique(df_tipo_parto_2021$Parto), na.last = TRUE)
 sort(unique(df_tipo_parto_aux$Parto), na.last = TRUE)
 
-df_tipo_parto_2022 <- full_join(df_tipo_parto_2021, df_tipo_parto_aux)
+df_tipo_parto_2023 <- full_join(df_tipo_parto_2021, df_tipo_parto_aux)
 
 # Exportando os dados 
-write.table(df_tipo_parto_2022, 'R/databases/Tipo_parto_muni2022.csv', sep = ";", dec = ".", row.names = FALSE)
+write.table(df_tipo_parto_2023, 'R/databases/Tipo_parto_muni2023.csv', sep = ";", dec = ".", row.names = FALSE)
 
 
 # Número de consultas de pré-natal ----------------------------------------
 df_consultas_2021 <- read.csv2("R/databases/Consultas_PreNatal_muni2021.csv")
 
-df_consultas_aux <- dados_preliminares_2022 |>
+df_consultas_aux <- dados_preliminares_2023 |>
   select(res_SIGLA_UF, res_MUNNOMEX, CODMUNRES, ano_nasc, CONSULTAS) |>
   mutate(nascidos = 1) |>
   group_by(res_SIGLA_UF, res_MUNNOMEX, CODMUNRES, ano_nasc, CONSULTAS) |>
@@ -173,16 +198,16 @@ names(df_consultas_aux) <- c('UF', 'Municipio', 'Codigo', 'Ano', 'Consultas', 'N
 sort(unique(df_consultas_2021$Consultas), na.last = TRUE)
 sort(unique(df_consultas_aux$Consultas), na.last = TRUE)
 
-df_consultas_2022 <- full_join(df_consultas_2021, df_consultas_aux)
+df_consultas_2023 <- full_join(df_consultas_2021, df_consultas_aux)
 
 # Exportando os dados 
-write.table(df_consultas_2022, 'R/databases/Consultas_PreNatal_muni2022.csv', sep = ";", dec = ".", row.names = FALSE)
+write.table(df_consultas_2023, 'R/databases/Consultas_PreNatal_muni2023.csv', sep = ";", dec = ".", row.names = FALSE)
 
 
 # Sexo fetal --------------------------------------------------------------
 df_sexo_fetal_2021 <- read.csv2("R/databases/Sexo_fetal_muni2021.csv")
 
-df_sexo_fetal_aux <- dados_preliminares_2022 |>
+df_sexo_fetal_aux <- dados_preliminares_2023 |>
   select(res_SIGLA_UF, res_MUNNOMEX, CODMUNRES, ano_nasc, SEXO) |>
   mutate(nascidos = 1) |>
   group_by(res_SIGLA_UF, res_MUNNOMEX, CODMUNRES, ano_nasc, SEXO) |>
@@ -194,16 +219,16 @@ names(df_sexo_fetal_aux) <- c('UF', 'Municipio', 'Codigo', 'Ano', 'Sexo', 'Nasci
 sort(unique(df_sexo_fetal_2021$Sexo), na.last = TRUE)
 sort(unique(df_sexo_fetal_aux$Sexo), na.last = TRUE)
 
-df_sexo_fetal_2022 <- full_join(df_sexo_fetal_2021, df_sexo_fetal_aux)
+df_sexo_fetal_2023 <- full_join(df_sexo_fetal_2021, df_sexo_fetal_aux)
 
 # Exportando os dados 
-write.table(df_sexo_fetal_2022, 'R/databases/Sexo_fetal_muni2022.csv', sep = ";", dec = ".", row.names = FALSE)
+write.table(df_sexo_fetal_2023, 'R/databases/Sexo_fetal_muni2023.csv', sep = ";", dec = ".", row.names = FALSE)
 
 
 # Apgar 1 -----------------------------------------------------------------
 df_apgar1_2021 <- read.csv2("R/databases/Apgar1_muni2021.csv")
 
-df_apgar1_aux <- dados_preliminares_2022 |>
+df_apgar1_aux <- dados_preliminares_2023 |>
   select(res_SIGLA_UF, res_MUNNOMEX, CODMUNRES, ano_nasc, APGAR1) |>
   mutate(nascidos = 1) |>
   group_by(res_SIGLA_UF, res_MUNNOMEX, CODMUNRES, ano_nasc, APGAR1) |>
@@ -215,16 +240,16 @@ names(df_apgar1_aux) <- c('UF', 'Municipio', 'Codigo', 'Ano', 'Apgar1', 'Nascido
 sort(unique(df_apgar1_2021$Apgar1), na.last = TRUE)
 sort(unique(df_apgar1_aux$Apgar1), na.last = TRUE)
 
-df_apgar1_2022 <- full_join(df_apgar1_2021, df_apgar1_aux)
+df_apgar1_2023 <- full_join(df_apgar1_2021, df_apgar1_aux)
 
 # Exportando os dados 
-write.table(df_apgar1_2022, 'R/databases/Apgar1_muni2022.csv', sep = ";", dec = ".", row.names = FALSE)
+write.table(df_apgar1_2023, 'R/databases/Apgar1_muni2023.csv', sep = ";", dec = ".", row.names = FALSE)
 
 
 # Apgar 5 -----------------------------------------------------------------
 df_apgar5_2021 <- read.csv2("R/databases/Apgar5_muni2021.csv")
 
-df_apgar5_aux <- dados_preliminares_2022 |>
+df_apgar5_aux <- dados_preliminares_2023 |>
   select(res_SIGLA_UF, res_MUNNOMEX, CODMUNRES, ano_nasc, APGAR5) |>
   mutate(nascidos = 1) |>
   group_by(res_SIGLA_UF, res_MUNNOMEX, CODMUNRES, ano_nasc, APGAR5) |>
@@ -236,16 +261,16 @@ names(df_apgar5_aux) <- c('UF', 'Municipio', 'Codigo', 'Ano', 'Apgar5', 'Nascido
 sort(unique(df_apgar5_2021$Apgar5), na.last = TRUE)
 sort(unique(df_apgar5_aux$Apgar5), na.last = TRUE)
 
-df_apgar5_2022 <- full_join(df_apgar5_2021, df_apgar5_aux)
+df_apgar5_2023 <- full_join(df_apgar5_2021, df_apgar5_aux)
 
 # Exportando os dados 
-write.table(df_apgar5_2022, 'R/databases/Apgar5_muni2022.csv', sep = ";", dec = ".", row.names = FALSE)
+write.table(df_apgar5_2023, 'R/databases/Apgar5_muni2023.csv', sep = ";", dec = ".", row.names = FALSE)
 
 
 # Anomalias ---------------------------------------------------------------
 df_anomalias_2021 <- read.csv2("R/databases/Anomalias_muni2021.csv")
 
-df_anomalias_aux <- dados_preliminares_2022 |>
+df_anomalias_aux <- dados_preliminares_2023 |>
   select(res_SIGLA_UF, res_MUNNOMEX, CODMUNRES, ano_nasc, IDANOMAL) |>
   mutate(nascidos = 1) |>
   group_by(res_SIGLA_UF, res_MUNNOMEX, CODMUNRES, ano_nasc, IDANOMAL) |>
@@ -257,16 +282,16 @@ names(df_anomalias_aux) <- c('UF', 'Municipio', 'Codigo', 'Ano', 'Anomalia', 'Na
 sort(unique(df_anomalias_2021$Anomalia), na.last = TRUE)
 sort(unique(df_anomalias_aux$Anomalia), na.last = TRUE)
 
-df_anomalias_2022 <- full_join(df_anomalias_2021, df_anomalias_aux)
+df_anomalias_2023 <- full_join(df_anomalias_2021, df_anomalias_aux)
 
 # Exportando os dados 
-write.table(df_anomalias_2022, 'R/databases/Anomalias_muni2022.csv', sep = ";", dec = ".", row.names = FALSE)
+write.table(df_anomalias_2023, 'R/databases/Anomalias_muni2023.csv', sep = ";", dec = ".", row.names = FALSE)
 
 
 # Peso fetal menor que 2500g ----------------------------------------------
 df_peso_fetal_2021 <- read.csv2("R/databases/Peso_menor_2500_muni2021.csv")
 
-df_peso_fetal_aux <- dados_preliminares_2022 |>
+df_peso_fetal_aux <- dados_preliminares_2023 |>
   select(res_SIGLA_UF, res_MUNNOMEX, CODMUNRES, ano_nasc, PESO) |>
   filter(PESO < 2500) |>
   mutate(nascidos = 1) |>
@@ -276,16 +301,16 @@ df_peso_fetal_aux <- dados_preliminares_2022 |>
 
 names(df_peso_fetal_aux) <- c('UF', 'Municipio', 'Codigo', 'Ano', 'Nascidos')
 
-df_peso_fetal_2022 <- full_join(df_peso_fetal_2021, df_peso_fetal_aux)
+df_peso_fetal_2023 <- full_join(df_peso_fetal_2021, df_peso_fetal_aux)
 
 # Exportando os dados 
-write.table(df_peso_fetal_2022, 'R/databases/Peso_menor_2500_muni2022.csv', sep = ";", dec = ".", row.names = FALSE)
+write.table(df_peso_fetal_2023, 'R/databases/Peso_menor_2500_muni2023.csv', sep = ";", dec = ".", row.names = FALSE)
 
 
 # Raça/cor da mãe ---------------------------------------------------------
 df_racamae_2021 <- read.csv2("R/databases/Raca_mae_muni2021.csv")
 
-df_racamae_aux <- dados_preliminares_2022 |>
+df_racamae_aux <- dados_preliminares_2023 |>
   select(res_SIGLA_UF, res_MUNNOMEX, CODMUNRES, ano_nasc, RACACORMAE) |>
   mutate(nascidos = 1) |>
   group_by(res_SIGLA_UF, res_MUNNOMEX, CODMUNRES, ano_nasc, RACACORMAE) |>
@@ -297,16 +322,16 @@ names(df_racamae_aux) <- c('UF', 'Municipio', 'Codigo', 'Ano', 'Raca_mae', 'Nasc
 sort(unique(df_racamae_2021$Raca_mae), na.last = TRUE)
 sort(unique(df_racamae_aux$Raca_mae), na.last = TRUE)
 
-df_racamae_2022 <- full_join(df_racamae_2021, df_racamae_aux)
+df_racamae_2023 <- full_join(df_racamae_2021, df_racamae_aux)
 
 # Exportando os dados 
-write.table(df_racamae_2022, 'R/databases/Raca_mae_muni2022.csv', sep = ";", dec = ".", row.names = FALSE)
+write.table(df_racamae_2023, 'R/databases/Raca_mae_muni2023.csv', sep = ";", dec = ".", row.names = FALSE)
 
 
 # Robson ------------------------------------------------------------------
 df_robson_2021 <- read.csv2("R/databases/Robson_muni2021.csv")
 
-df_robson_aux <- dados_preliminares_2022 |>
+df_robson_aux <- dados_preliminares_2023 |>
   select(res_SIGLA_UF, res_MUNNOMEX, CODMUNRES, ano_nasc, TPROBSON) |>
   mutate(nascidos = 1) |>
   group_by(res_SIGLA_UF, res_MUNNOMEX, CODMUNRES, ano_nasc, TPROBSON) |>
@@ -318,16 +343,16 @@ names(df_robson_aux) <- c('UF', 'Municipio', 'Codigo', 'Ano', 'Robson', 'Nascido
 sort(unique(df_robson_2021$Robson), na.last = TRUE)
 sort(unique(df_robson_aux$Robson), na.last = TRUE)
 
-df_robson_2022 <- full_join(df_robson_2021, df_robson_aux)
+df_robson_2023 <- full_join(df_robson_2021, df_robson_aux)
 
 # Exportando os dados 
-write.table(df_robson_2022, 'R/databases/Robson_muni2022.csv', sep = ";", dec = ".", row.names = FALSE)
+write.table(df_robson_2023, 'R/databases/Robson_muni2023.csv', sep = ";", dec = ".", row.names = FALSE)
 
 
 # Parto prematuro ---------------------------------------------------------
 df_prematuro_pcdas_2021 <- read.csv2("R/databases/Prematuro_PCDAS_muni2021.csv")
 
-df_prematuro_pcdas_aux <- dados_preliminares_2022 |>
+df_prematuro_pcdas_aux <- dados_preliminares_2023 |>
   select(res_SIGLA_UF, res_MUNNOMEX, CODMUNRES, ano_nasc, GESTACAO) |>
   mutate(nascidos = 1) |>
   group_by(res_SIGLA_UF, res_MUNNOMEX, CODMUNRES, ano_nasc, GESTACAO) |>
@@ -339,16 +364,16 @@ names(df_prematuro_pcdas_aux) <- c('UF', 'Municipio', 'Codigo', 'Ano', 'Gestacao
 sort(unique(df_prematuro_pcdas_2021$Gestacao), na.last = TRUE)
 sort(unique(df_prematuro_pcdas_aux$Gestacao), na.last = TRUE)
 
-df_prematuro_pcdas_2022 <- full_join(df_prematuro_pcdas_2021, df_prematuro_pcdas_aux)
+df_prematuro_pcdas_2023 <- full_join(df_prematuro_pcdas_2021, df_prematuro_pcdas_aux)
 
 # Exportando os dados 
-write.table(df_prematuro_pcdas_2022, 'R/databases/Prematuro_PCDAS_muni2022.csv', sep = ";", dec = ".", row.names = FALSE)
+write.table(df_prematuro_pcdas_2023, 'R/databases/Prematuro_PCDAS_muni2023.csv', sep = ";", dec = ".", row.names = FALSE)
 
 
 # Cesárea eletiva ---------------------------------------------------------
 df_ces_2021 <- read.csv2("R/databases/Cesaria_antes_do_parto_muni2021.csv")
 
-df_ces_aux <- dados_preliminares_2022 |>
+df_ces_aux <- dados_preliminares_2023 |>
   select(res_SIGLA_UF, res_MUNNOMEX, CODMUNRES, ano_nasc, TPROBSON, STCESPARTO) |>
   filter(TPROBSON %in% c(2, 4)) |>
   mutate(nascidos = 1) |>
@@ -361,16 +386,16 @@ names(df_ces_aux) <- c('UF', 'Municipio', 'Codigo', 'Ano', 'Robson', 'cesarea_an
 sort(unique(df_ces_2021$cesarea_antes_do_parto), na.last = TRUE)
 sort(unique(df_ces_aux$cesarea_antes_do_parto), na.last = TRUE)
 
-df_ces_2022 <- full_join(df_ces_2021, df_ces_aux)
+df_ces_2023 <- full_join(df_ces_2021, df_ces_aux)
 
 # Exportando os dados
-write.table(df_ces_2022, 'R/databases/Cesaria_antes_do_parto_muni2022.csv', sep = ";", dec = ".", row.names = FALSE)
+write.table(df_ces_2023, 'R/databases/Cesaria_antes_do_parto_muni2023.csv', sep = ";", dec = ".", row.names = FALSE)
 
 
 # Partos induzidos --------------------------------------------------------
 df_parto_induzido_2021 <- read.csv2("R/databases/Parto_induzido_muni2021.csv")
 
-df_parto_induzido_aux <- dados_preliminares_2022 |>
+df_parto_induzido_aux <- dados_preliminares_2023 |>
   select(res_SIGLA_UF, res_MUNNOMEX, CODMUNRES, ano_nasc, TPROBSON, STTRABPART) |>
   filter(TPROBSON %in% c(2, 4)) |>
   mutate(nascidos = 1) |>
@@ -383,16 +408,16 @@ names(df_parto_induzido_aux) <- c('UF','Municipio', 'Codigo', 'Ano', 'Robson', '
 sort(unique(df_parto_induzido_2021$parto_induzido), na.last = TRUE)
 sort(unique(df_parto_induzido_aux$parto_induzido), na.last = TRUE)
 
-df_parto_induzido_2022 <- full_join(df_parto_induzido_2021, df_parto_induzido_aux)
+df_parto_induzido_2023 <- full_join(df_parto_induzido_2021, df_parto_induzido_aux)
 
 # Exportando os dados
-write.table(df_parto_induzido_2022, 'R/databases/Parto_induzido_muni2022.csv', sep = ";", dec = ".", row.names = FALSE)
+write.table(df_parto_induzido_2023, 'R/databases/Parto_induzido_muni2023.csv', sep = ";", dec = ".", row.names = FALSE)
 
 
 # Prematuridade consultas -------------------------------------------------
 df_prematuridade_consultas_2021 <- read.csv2("R/databases/Prematuridade_consultas_muni2021.csv")
 
-df_prematuridade_consultas_aux <- dados_preliminares_2022 |>
+df_prematuridade_consultas_aux <- dados_preliminares_2023 |>
   select(res_SIGLA_UF, res_MUNNOMEX, CODMUNRES, ano_nasc, GESTACAO, CONSULTAS) |>
   mutate(nascidos = 1) |>
   group_by(res_SIGLA_UF, res_MUNNOMEX, CODMUNRES, ano_nasc, GESTACAO, CONSULTAS) |>
@@ -407,16 +432,16 @@ sort(unique(df_prematuridade_consultas_aux$Gestacao), na.last = TRUE)
 sort(unique(df_prematuridade_consultas_2021$Consultas), na.last = TRUE)
 sort(unique(df_prematuridade_consultas_aux$Consultas), na.last = TRUE)
 
-df_prematuridade_consultas_2022 <- full_join(df_prematuridade_consultas_2021, df_prematuridade_consultas_aux)
+df_prematuridade_consultas_2023 <- full_join(df_prematuridade_consultas_2021, df_prematuridade_consultas_aux)
 
 # Exportando os dados 
-write.table(df_prematuridade_consultas_2022, 'R/databases/Prematuridade_consultas_muni2022.csv', sep = ";", dec = ".", row.names = FALSE)
+write.table(df_prematuridade_consultas_2023, 'R/databases/Prematuridade_consultas_muni2023.csv', sep = ";", dec = ".", row.names = FALSE)
 
 
 # Prematuros, cesáreas e Robson -------------------------------------------
 df_robson_cesar_2021 <- read.csv2("R/databases/Robson_cesar_muni2021.csv")
 
-df_robson_cesar_aux <- dados_preliminares_2022 |>
+df_robson_cesar_aux <- dados_preliminares_2023 |>
   select(res_SIGLA_UF, res_MUNNOMEX, CODMUNRES, ano_nasc, TPROBSON, PARTO) |>
   mutate(nascidos = 1) |>
   group_by(res_SIGLA_UF, res_MUNNOMEX, CODMUNRES, ano_nasc, TPROBSON, PARTO) |>
@@ -425,8 +450,8 @@ df_robson_cesar_aux <- dados_preliminares_2022 |>
 
 names(df_robson_cesar_aux) <- c('UF', 'Municipio', 'Codigo', 'Ano', 'Robson', 'Parto', 'Nascidos')
 
-df_robson_cesar_2022 <- full_join(df_robson_cesar_2021, df_robson_cesar_aux)
+df_robson_cesar_2023 <- full_join(df_robson_cesar_2021, df_robson_cesar_aux)
 
 # Exportando os dados (Nascimentos_muni = UF, Município, Ano e Nascimentos)
-write.table(df_robson_cesar_2022, 'R/databases/Robson_cesar_muni2022.csv', sep = ";", dec = ".", row.names = FALSE)
+write.table(df_robson_cesar_2023, 'R/databases/Robson_cesar_muni2023.csv', sep = ";", dec = ".", row.names = FALSE)
 
